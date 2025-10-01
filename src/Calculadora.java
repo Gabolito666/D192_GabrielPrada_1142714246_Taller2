1742801b55b77470397010bc7abf6a301ea450d3
Gabolito666 <gabolito108@gmail.com>
2025-09-30 21:15:28 -0500

    Initial commit

---
+++ src/Calculadora.java
@@ -0,0 +1,48 @@
+public class Calculadora {
+
+    // Atributos
+    private float num1;
+    private float num2;
+
+    // Metodos
+    public float sumar() {
+        return num1 + num2;
+    }
+
+    public float restar() {
+        return num1 - num2;
+    }
+
+    public float multiplicar() {
+        return num1 * num2;
+    }
+
+    public float dividir() {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            System.out.println("Error: Division por cero");
+            return 0;
+        }
+    }
+
+    public double potencia() {
+        return Math.pow(num1, num2);
+    }
+
+    public float getNum1() {
+        return num1;
+    }
+
+    public void setNum1(float num1) {
+        this.num1 = num1;
+    }
+
+    public float getNum2() {
+        return num2;
+    }
+
+    public void setNum2(float num2) {
+        this.num2 = num2;
+    }
+}
---
+++ src/InterfazGraficaCalculadora.java
@@ -0,0 +1,2 @@
+public class InterfazGraficaCalculadora {
+}
